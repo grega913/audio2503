4ec3069db9dafc6382cf1e7191c4e1961021666c
Grega S <grega913@gmail.com>
2025-03-23 20:23:14 +0100

    style: Adjust message header and content spacing in chat UI

--- static/my_web_app/js/lang.js
+++ static/my_web_app/js/lang.js
@@ -13,7 +13,7 @@ function createMessageElement(json) {
   const message = document.createElement("li");
   message.innerHTML = `
         <div class="message-header">
-            <div class="message-type">${type.toUpperCase()}</div>
+            <div class="message-type">${type()}</div>
             <div class="message-timestamp">${new Date(timestamp).toLocaleTimeString()}</div>
         </div>
         <div class="message-content">${content}</div>
